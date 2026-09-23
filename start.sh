727338f8f9ee173f5c35bf2e63b161456fd1424f
Priya Kumari <chaudhary21priya@gmail.com>
2026-09-23 17:37:52 +0530

    add the timeouts for workers

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo "Starting Celery worker in background..."
 celery -A config worker -l info &
 
 echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2
+exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --timeout 120git 
