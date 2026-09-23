341eef00551da767a45d052fca0c7462c0216750
Priya Kumari <chaudhary21priya@gmail.com>
2026-09-23 15:15:43 +0530

    create start.sh file

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Starting Celery worker in background..."
+celery -A config worker -l info --pool=solo &
+
+echo "Starting Gunicorn..."
+exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 3
